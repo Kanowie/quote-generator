2d469694f4da24e5b0cb4c3537312f03add90554
Kanowie <n.abdo613@gmail.com>
2021-02-18 13:43:06 -0500

    An array of one liners. Need to make them randomly generate now

--- cheesyjoke.js
+++ cheesyjoke.js
@@ -1,3 +1,6 @@
-const knock = () => {
-    console.log("knock knock")
+let oneLiners = ['I ate a clock yesterday, it was very time-consuming.', 'Have you played the updated kids game? I Spy With My Little Eye. . .Phone.', 'A perfectionist walked into a bar...apparently, the bar wasn’t set high enough.']
+
+function badJokes(bad) {
+    var result = '';
+
 }
